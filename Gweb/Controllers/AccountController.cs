0aa071d3855ffcc91801c6b16105d2b9367155bc
K John <Jonathan6703@gmail.com>
2023-04-17 09:43:13 -0400

    update

--- Gweb/Controllers/AccountController.cs
+++ Gweb/Controllers/AccountController.cs
@@ -31,33 +31,7 @@ namespace Gweb.Controllers
             return Redirect(Request.UrlReferrer.ToString());
         }
 
-        public SiteSession CurrentSiteSession
-        {
-            get
-            {
-                SiteSession shopSession = (SiteSession)this.Session["SiteSession"];
-                return shopSession;
-            }
-        }
-        protected override void ExecuteCore()
-        {
-            int culture = 0;
-            if (this.Session == null || this.Session["CurrentUICulture"] == null)
-            {
-                int.TryParse(System.Configuration.ConfigurationManager.AppSettings["Culture"], out culture);
-                this.Session["CurrentUICulture"] = culture;
-            }
-            else
-            {
-                culture = (int)this.Session["CurrentUICulture"];
-            }
-            //
-            SiteSession.CurrentUICulture = culture;
-            //
-            // Invokes the action in the current controller context.
-            //
-            base.ExecuteCore();
-        }
+       
 
     }
 }
---
+++ Gweb/Controllers/BaseController.cs
@@ -0,0 +1,42 @@
+﻿using Gweb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gweb.Controllers
+{
+    public class BaseController : Controller
+    {
+        // GET: Base
+        
+        public SiteSession CurrentSiteSession
+        {
+            get
+            {
+                SiteSession shopSession = (SiteSession)this.Session["SiteSession"];
+                return shopSession;
+            }
+        }
+        protected override void ExecuteCore()
+        {
+            int culture = 0;
+            if (this.Session == null || this.Session["CurrentUICulture"] == null)
+            {
+                int.TryParse(System.Configuration.ConfigurationManager.AppSettings["Culture"], out culture);
+                this.Session["CurrentUICulture"] = culture;
+            }
+            else
+            {
+                culture = (int)this.Session["CurrentUICulture"];
+            }
+            //
+            SiteSession.CurrentUICulture = culture;
+            //
+            // Invokes the action in the current controller context.
+            //
+            base.ExecuteCore();
+        }
+    }
+}
